29755eef2a4b80ca2e764df5efa7d7cfe821f24b
Fallbringer9 <oresteemmanuel@gmail.com>
2026-02-26 00:27:04 +0100

    feat(infra): add FrontendStack with S3 private bucket and CloudFront distribution

--- infra/app.py
+++ infra/app.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
 from stacks.backend_stack import BackendStack
+from stacks.frontend_stack import FrontendStack
 
 app = cdk.App()
 
 BackendStack(
     app,
     "BackendStack",
+)
+FrontendStack(
+    app,
+    "FrontendStack",
 )
 
 app.synth()
---
+++ infra/stacks/frontend_stack.py
@@ -0,0 +1,67 @@
+from aws_cdk import (
+    Stack,
+    CfnOutput,
+    RemovalPolicy,
+    Duration,
+    aws_s3 as s3,
+    aws_cloudfront as cloudfront,
+    aws_cloudfront_origins as origins,
+)
+from constructs import Construct
+
+
+class FrontendStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        bucket = s3.Bucket(
+            self,
+            "FrontendBucket",
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            enforce_ssl=True,
+            versioned=False,
+            removal_policy=RemovalPolicy.RETAIN,
+            auto_delete_objects=False,
+        )
+
+        # OAC (Origin Access Control)
+        oac = cloudfront.S3OriginAccessControl(
+            self,
+            "FrontendOAC",
+            signing=cloudfront.Signing.SIGV4_ALWAYS,
+        )
+
+        origin = origins.S3BucketOrigin.with_origin_access_control(
+            bucket,
+            origin_access_control=oac,
+        )
+
+        dist = cloudfront.Distribution(
+            self,
+            "FrontendDistribution",
+            default_behavior=cloudfront.BehaviorOptions(
+                origin=origin,
+                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                cache_policy=cloudfront.CachePolicy.CACHING_OPTIMIZED,
+                compress=True,
+            ),
+            default_root_object="index.html",
+            error_responses=[
+                cloudfront.ErrorResponse(
+                    http_status=403,
+                    response_http_status=200,
+                    response_page_path="/index.html",
+                    ttl=Duration.minutes(1),
+                ),
+                cloudfront.ErrorResponse(
+                    http_status=404,
+                    response_http_status=200,
+                    response_page_path="/index.html",
+                    ttl=Duration.minutes(1),
+                ),
+            ],
+        )
+
+        CfnOutput(self, "FrontendBucketName", value=bucket.bucket_name)
+        CfnOutput(self, "CloudFrontDomainName", value=dist.domain_name)
